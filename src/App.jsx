32c4fd279aa038c0c155da40c114cb6a11f68cdb
Nick-Kevin <nicblou23@gmail.com>
2024-08-14 09:57:44 +0300

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import VoiceRecognition from './components/VoiceRecognition'
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route exact path="/Pergola-shelter-safe-web-app" element={<h1>Home Page</h1>} />
+          <Route exact path="/Pergola-shelter-safe-web-app" element={<Home />} />
           <Route exact path="/Pergola-shelter-safe-web-app/voice-recognition" element={<div><h1>Hey there</h1> <VoiceRecognition /></div>} />
         </Routes>        
       </Router>      
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+
+const Navbar = () => {
+  // State to manage the navbar's visibility
+  const [nav, setNav] = useState(false);
+
+  // Toggle function to handle the navbar's display
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  // Array containing navigation items
+  const navItems = [
+    { id: 1, text: 'Accueil' },
+    { id: 2, text: 'Commande vocale' },
+    { id: 3, text: 'Commande mannuelle' },
+    { id: 4, text: 'Caméra' },
+    { id: 5, text: 'Volets' },
+  ];
+
+  return (
+    <div className='bg-black flex justify-between items-center h-24 mx-auto px-4 text-white'>
+      {/* Logo */}
+      <h1 className='w-full xl:w-5/12 text-3xl font-bold text-[#00df9a]'>Pergola.</h1>
+
+      {/* Desktop Navigation */}
+      <ul className='hidden md:flex xl:pr-24'>
+          <li
+            className='p-4 hover:bg-[#00df9a] rounded-xl md:text-sm m-2 cursor-pointer duration-300 hover:text-black'
+          >
+            Accueil
+          </li>
+          <li
+            className='p-4 hover:bg-[#00df9a] rounded-xl md:text-sm m-2 cursor-pointer duration-300 hover:text-black'
+          >
+            Commande vocale
+          </li>
+          <li
+            className='p-4 hover:bg-[#00df9a] rounded-xl md:text-sm m-2 cursor-pointer duration-300 hover:text-black'
+          >
+            Commande mannuelle
+          </li>
+          <li
+            className='p-4 hover:bg-[#00df9a] rounded-xl md:text-sm m-2 cursor-pointer duration-300 hover:text-black'
+          >
+            Caméra
+          </li>
+          <li
+            className='p-4 hover:bg-[#00df9a] rounded-xl md:text-sm m-2 cursor-pointer duration-300 hover:text-black'
+          >
+            Volets
+          </li>
+      </ul>
+
+      {/* Mobile Navigation Icon */}
+      <div onClick={handleNav} className='block md:hidden'>
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+
+      {/* Mobile Navigation Menu */}
+      <ul
+        className={
+          nav
+            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
+            : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+        }
+      >
+        {/* Mobile Logo */}
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Pergola.</h1>
+
+        {/* Mobile Navigation Items */}
+          <li
+            className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
+          >
+            Accueil
+          </li>
+          <li
+            className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
+          >
+            Commande vocale
+          </li>
+          <li
+            className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
+          >
+            Commande mannuelle
+          </li>
+          <li
+            className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
+          >
+            Caméra
+          </li>
+          <li
+            className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
+          >
+            Volets
+          </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
