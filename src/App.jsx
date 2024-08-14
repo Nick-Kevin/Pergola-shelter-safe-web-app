25290adea003ce5a76159ea15b855c9e3a059d15
Nick-Kevin <nicblou23@gmail.com>
2024-08-14 09:33:54 +0300

    set router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import VoiceRecognition from './components/VoiceRecognition'
 
 function App() {
   return (
     <>
-      <h1>Hey there</h1>
-      <VoiceRecognition />
+      <Router>
+        <Routes>
+          <Route exact path="/Pergola-shelter-safe-web-app" element={<h1>Home Page</h1>} />
+          <Route exact path="/Pergola-shelter-safe-web-app/voice-recognition" element={<div><h1>Hey there</h1> <VoiceRecognition /></div>} />
+        </Routes>        
+      </Router>      
     </>
   )
 }
