8ad465044df6960645072b76a2ed24b161376e90
Sébastien Randriamanana <sebastienrandriamanana3@gmail.com>
2024-08-26 10:35:28 +0300

    changes

--- src/pages/TimeCommand.jsx
+++ src/pages/TimeCommand.jsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import { FiClock } from "react-icons/fi";
 
 const TimeCommand = ()=>{
 
     const [turnOnTime, setTurnOnTime] = useState('')
     const [turnOffTime, setTurnOffTime] = useState('')
 
+
     function handleSubmit(e){
+        console.log(turnOnTime, turnOffTime)
         e.preventDefault();
         async function sendData(){
             await axios.post('http://192.168.10.106/setTimes',{turnOnTime, turnOffTime}
@@ -21,23 +24,27 @@ const TimeCommand = ()=>{
     }   
 
 
+
 return(
     <>
-    <form onSubmit={handleSubmit} className="w-80 flex flex-col content-center">
-        <div className="flex justify-around mb-10 ">
+    <form onSubmit={handleSubmit} className="w-96 flex flex-col items-center">
+        <div className="w-full flex justify-around mb-10 ">
             <label htmlFor="">Heure d'allumage :</label>
-            <input type="time"
+            <input type="time" 
                 value={turnOnTime}
                 onChange={(e)=>setTurnOnTime(e.target.value+":00")}
              />
+           
         </div>
 
-        <div className=" flex justify-around mb-10 ">
+        <div className="w-full flex justify-around mb-10 ">
             <label htmlFor="">Heure d'extinction :</label>
-            <input type="time"
+             <input type="time"
                 value={turnOffTime}
                 onChange={(e)=>setTurnOffTime(e.target.value+":00")}
              />
+            
+            
         </div>
         <div className="mb-5 flex justify-center">
             <button className="shadow-lg text-white bg-[#060606]" type="submit">
